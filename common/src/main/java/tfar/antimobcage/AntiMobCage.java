d77763baa80680e76799f2b010ea9148446fcc0c
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-04-25 01:03:06 -0400

    boilerplate

---
+++ common/src/main/java/tfar/antimobcage/AntiMobCage.java
@@ -0,0 +1,13 @@
+package tfar.antimobcage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AntiMobCage {
+
+    public static final String MOD_ID = "antimobcage";
+    public static final String MOD_NAME = "AntiMobCage";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+    public static void init() {
+    }
+}
---
+++ forge/src/main/java/tfar/antimobcage/AntiMobCageForge.java
@@ -0,0 +1,11 @@
+package tfar.antimobcage;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(AntiMobCage.MOD_ID)
+public class AntiMobCageForge {
+    
+    public AntiMobCageForge() {
+        AntiMobCage.init();
+    }
+}
